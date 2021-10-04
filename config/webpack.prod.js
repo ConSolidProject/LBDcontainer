818fcea08fc56edadc7cca233f178db4d12b118c
jmauwerb <jeroen.werbrouck@ugent.be>
2021-10-04 12:28:52 +0200

    first commit

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -9,7 +9,7 @@ const prodConfig = {
     mode: 'production',
     output: {
         filename: '[name].[contenthash].js',
-        publicPath: packageJson.domain
+        publicPath: packageJson.domain 
     },
     plugins: [
         new ModuleFederationPlugin({
