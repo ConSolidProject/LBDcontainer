503b7225e30a39cb5ae1ed368599b7e3ecef639a
jmauwerb <jeroen.werbrouck@ugent.be>
2021-10-06 10:48:31 +0200

    new deploy with right store

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {atom, atomFamily, selectorFamily} from 'recoil'
+import {atom} from 'recoil'
 
 const config = atom({
     key: "config", 
@@ -23,7 +23,7 @@ const config = atom({
               "icon": "M14 2H6c-1.1 0-1.99.9-1.99 2L4 20c0 1.1.89 2 1.99 2H18c1.1 0 2-.9 2-2V8l-6-6zm2 16H8v-2h8v2zm0-4H8v-2h8v2zm-3-5V3.5L18.5 9H13z"
             },
             "projectquery": {
-              "url": "http://localhost:8083/remoteEntry.js",
+              "url": "https://consolidproject.github.io/pluginQuery/remoteEntry.js",
               "scope": "projectquery",
               "module": "./index",
               "icon": "M7 9H2V7h5v2zm0 3H2v2h5v-2zm13.59 7-3.83-3.83c-.8.52-1.74.83-2.76.83-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5c0 1.02-.31 1.96-.83 2.75L22 17.59 20.59 19zM17 11c0-1.65-1.35-3-3-3s-3 1.35-3 3 1.35 3 3 3 3-1.35 3-3zM2 19h10v-2H2v2z"
@@ -31,7 +31,7 @@ const config = atom({
           }
         },
         "viewer": {
-          "url": "http://localhost:8082/remoteEntry.js",
+          "url": "https://consolidproject.github.io/pluginViewer/remoteEntry.js",
           "scope": "viewer",
           "module": "./index",
           "dimensions": {
@@ -42,7 +42,7 @@ const config = atom({
           }
         },
             "imageannotator": {
-              "url": "http://localhost:8084/remoteEntry.js",
+              "url": "https://consolidproject.github.io/pluginImageViewer/remoteEntry.js",
               "scope": "imageannotator",
               "module": "./index",
               "dimensions": {
